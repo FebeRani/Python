80544adf3e7b73e74449a426236ba17d36494054
FebeRani <feberani7@gmail.com>
2022-04-13 10:06:16 +0530

    dictonary-commit

---
+++ set.py
@@ -0,0 +1,16 @@
+myset={1,35,356,344,34,1,35,35,"Raj"}
+
+# set is unordered
+# set cannot have duplicates
+
+print(myset)
+
+myset.add(135)
+
+print(myset)
+
+for x in myset:
+    print(x)
+
+    myset.sort()
+    print(myset)
